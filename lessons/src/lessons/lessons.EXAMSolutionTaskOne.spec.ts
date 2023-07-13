7ee9761b166a5dc6a70c3584f39d7918e2298c9f
Adrian Eguez <eadepto@hotmail.com>
2023-07-13 09:40:18 -0500

    feat: unit tests and boilerplate

---
+++ lessons/src/lessons/lessons.EXAMSolutionTaskOne.spec.ts
@@ -0,0 +1,15 @@
+import { Lessons } from './lessons';
+
+describe('Tests', () => {
+  describe('solutionNumberIntegersDivisible', () => {
+    const instance: Lessons = new Lessons();
+    beforeEach(() => {
+      console.info('Initializing');
+    });
+    it('A: 1 => 1', () => {
+      expect(instance).toBeDefined();
+      const response = instance.EXAMSolutionTaskOne(1);
+      expect(response).toBe(1);
+    });
+  });
+});
--- lessons/src/lessons/lessons.ts
+++ lessons/src/lessons/lessons.ts
@@ -733,39 +733,21 @@ each element of array A is an integer within the range [0..2,147,483,647].
   }
 
   /*
-  * 
-  * Write a function
+   *  * */
 
-class Solution { public int solution(int[] A); }
+  EXAMSolutionTaskOne(A: number): number {
+    return A >= 0 ? 1 : 0;
+  }
+  /*
+   *  * */
 
-that, given an array A consisting of N integers, returns the number of distinct values in array A.
+  EXAMSolutionTaskTwo(A: number): number {
+    return A >= 0 ? 1 : 0;
+  }
+  /*
+   *  * */
 
-For example, given array A consisting of six elements such that:
-
- A[0] = 2    A[1] = 1    A[2] = 1
- A[3] = 2    A[4] = 3    A[5] = 1
-the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
-
-Write an efficient algorithm for the following assumptions:
-
-N is an integer within the range [0..100,000];
-each element of array A is an integer within the range [−1,000,000..1,000,000].Write a function
-
-class Solution { public int solution(int[] A); }
-
-that, given an array A consisting of N integers, returns the number of distinct values in array A.
-
-For example, given array A consisting of six elements such that:
-
- A[0] = 2    A[1] = 1    A[2] = 1
- A[3] = 2    A[4] = 3    A[5] = 1
-the function should return 3, because there are 3 distinct values appearing in array A, namely 1, 2 and 3.
-
-Write an efficient algorithm for the following assumptions:
-
-N is an integer within the range [0..100,000];
-each element of array A is an integer within the range [−1,000,000..1,000,000].
-  * */
-
-  EXAMSolutionTaskOne() {}
+  EXAMSolutionTaskThree(A: number): number {
+    return A >= 0 ? 1 : 0;
+  }
 }
